7bf05fe714511311e95733d7ac042587a676b43c
NathmaOmary <nathmaomary41@gmail.com>
2024-12-31 04:10:33 -0800

    creating shopping cart

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -72,3 +72,55 @@ std::string User::getUsername() const {
 
 
 
+// ShoppingCart.h
+#ifndef SHOPPINGCART_H
+#define SHOPPINGCART_H
+
+#include <vector>
+#include "Product.h"
+
+class ShoppingCart {
+public:
+    void addProduct(const Product& product);
+    void removeProduct(int productId);
+    double calculateTotal() const;
+    void displayCart() const;
+
+private:
+    std::vector<Product> products;
+};
+
+#endif
+
+// ShoppingCart.cpp
+#include "ShoppingCart.h"
+#include <iostream>
+
+void ShoppingCart::addProduct(const Product& product) {
+    products.push_back(product);
+}
+
+void ShoppingCart::removeProduct(int productId) {
+    for (auto it = products.begin(); it != products.end(); ++it) {
+        if (it->getId() == productId) {
+            products.erase(it);
+            break;
+        }
+    }
+}
+
+double ShoppingCart::calculateTotal() const {
+    double total = 0;
+    for (const auto& product : products) {
+        total += product.getPrice();
+    }
+    return total;
+}
+
+void ShoppingCart::displayCart() const {
+    std::cout << "Products in Cart: \n";
+    for (const auto& product : products) {
+        std::cout << "ID: " << product.getId() << ", Name: " << product.getName() 
+                  << ", Price: $" << product.getPrice() << std::endl;
+    }
+}
